758530b414e1e3895bcc1c6f74c4e502e86be94e
juniaskoudou <juniashk@gmail.com>
2026-02-11 19:33:17 +0100

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+      <Button class="bg-red-500 text-white">Click me</Button>
+    </div>
+  </template>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
